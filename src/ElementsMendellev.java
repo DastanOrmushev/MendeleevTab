2a6bebd774ecd3aed4b0507722fb45f8872ee988
dormushev <orm_das@mail.ru>
2023-08-14 23:55:28 +0600

    Second try to commit and push

--- src/ElementsMendellev.java
+++ src/ElementsMendellev.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public enum ElementsMendellev {
+public enum ElementsMendellev {
+    H(1,"Водород");
+    private final int num;
+    private final String name;
+
+    public int getNum() {
+        return num;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    ElementsMendellev(int num, String name) {
+        this.num = num;
+        this.name = name;
+    }
 }
