a3c8441706932545fd2dbdf45e1158ea7371c72b
dormushev <orm_das@mail.ru>
2023-08-15 11:20:18 +0600

    fix

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         for (ElementsMendellev elements:ElementsMendellev.values()) {
-            System.out.println("Элемент: " +elements.getName()+"; Периодический номер: "+elements.getNum());
+            System.out.println("Элемент: " +elements.getName()+";  Периодический номер: "+elements.getNum());
         }
     }
 }
